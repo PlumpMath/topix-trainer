046d38cf350812efec0c5ae0d25b14ca4301de00
rod <rod@pu-gh.com>
2012-04-21 17:05:52 +0100

    updated topix to 0.0.5

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (defproject topix-trainer "0.0.1"
   :description "Training resources for Topix"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [topix "0.0.3"]
+                 [topix "0.0.5"]
                  [getweets "1.0.0"]
                  [cheshire "4.0.0"]]
   :main topix-trainer.core)
