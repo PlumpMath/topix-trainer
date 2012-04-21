59c9a386b3e286fa1d8aaa44002a2a13f3bb25da
rod <rod@pu-gh.com>
2012-04-21 10:29:43 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+
+(defproject topix-trainer "0.0.1"
+  :description "Training resources for Topix"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [topix "0.0.2"]
+                 [getweets "1.0.0"]
+                 [cheshire "4.0.0"]]
+  :main topix-trainer.core)
+
---
+++ src/topix_trainer/core.clj
@@ -0,0 +1,52 @@
+
+(ns topix-trainer.core
+  (:import (java.io File))
+  (:use cheshire.core)
+  (:require [topix.init :as init]
+            [topix.data :as data]
+            [getweets.core :as getweets]))
+
+(defn- train-with
+  "Loading training data from the specified file"
+  [topic file]
+  (let [data (parse-string (slurp file) true)]
+    (doseq [text data]
+      (data/analyse topic text))))
+
+(defn- train
+  []
+  (init/mongo)
+  (let [dir (File. "data/tweets")]
+    (doseq [topic (.list dir)]
+      (doseq [file (.list (File. (format "%s/%s" dir topic)))]
+        (println "Loading: " file)
+        (train-with topic 
+                    (format "%s/%s/%s" dir topic file))))))
+
+(defn- fetch-tweets
+  [topic account]
+  (let [dir (File. (format "data/tweets/%s" topic))
+        path (format "%s/%s.json" dir account)
+        data (->> (getweets/fetch account)
+                  (map #(:text %)))]
+    (println "Writing tweets to:" path)
+    (if (not (.exists dir))
+        (.mkdirs dir))
+    (spit path (generate-string data))))
+
+(defn- usage
+  []
+  (prn "Usage: lein run train|fetch-tweets"))
+
+(defn- dispatch
+  [action args]
+  (condp = action
+    "train" (train)
+    "fetch-tweets" (apply fetch-tweets args)
+    (usage)))
+
+;; Main
+
+(defn -main [& [action & args]]
+  (dispatch action args))
+
